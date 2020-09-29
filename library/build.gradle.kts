8014f4deff39fb67af02f0d475cba03b7b1fcd40
Peter van Rensburg <pvanrensburg@figure.com>
2020-09-29 16:08:22 -0600

    upgrade to KotlinX 1.0.0-RC2 and force outputVariant

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -27,6 +27,9 @@ dependencies {
 
 java {
     withSourcesJar()
+
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
