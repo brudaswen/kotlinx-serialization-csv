e2390c2c0e7ef2be0e24f5caeb1f0533441f11b7
Joseph Ivie <joseph@lightningkite.com>
2023-01-13 13:20:39 -0700

    Removed commented debugging println's for FetchSource

--- library/src/main/kotlin/kotlinx/serialization/csv/decode/FetchSource.kt
+++ library/src/main/kotlin/kotlinx/serialization/csv/decode/FetchSource.kt
@@ -31,32 +31,26 @@ class FetchSource(private val getChar: ()->Char?): Source {
     override fun canRead(): Boolean = offset <= nextPosition
 
     override fun read(): Char? {
-        // println("read $offset")
         if(offset > nextPosition) {
             return null
         } else if(offset == nextPosition) {
             if(next == null) {
                 offset++
-                // println("hit the end")
                 if(marks.isEmpty()) queue.clear()
                 return null
             }
             val c = nextChar()
             if(marks.isNotEmpty()) {
-                // println("Adding $c to the queue")
                 if(queue.isEmpty()) {
-                    // println("Queue offset set to $offset")
                     queueOffset = offset
                 }
                 queue.add(c)
             } else {
-                // println("Queue cleared due to read after end with no marks")
                 queue.clear()
             }
             offset++
             return c
         } else {
-            // println("Reading $offset from queue: ${queue} at index ${offset-queueOffset} due to offset ${queueOffset}")
             val indexToCheck = offset-queueOffset
             val result = queue[indexToCheck]
             offset++
@@ -65,33 +59,25 @@ class FetchSource(private val getChar: ()->Char?): Source {
     }
 
     override fun peek(): Char? {
-        // println("peek $offset")
         if(offset > nextPosition) {
             return null
         } else if(offset == nextPosition) {
             return next
         } else {
-            // println("Peeking $offset from queue: ${queue} at index ${offset-queueOffset} due to offset ${queueOffset}")
             return queue[offset-queueOffset]
         }
     }
 
     override fun mark() {
-        // println("mark $offset")
         marks.add(offset)
-        // println("Marks: $marks")
     }
 
     override fun unmark() {
-        // println("unmark $offset")
         marks.removeAt(marks.lastIndex)
-        // println("Marks: $marks")
     }
 
     override fun reset() {
-        // println("reset $offset")
         offset = marks[marks.lastIndex]
         marks.removeAt(marks.lastIndex)
-        // println("Marks: $marks, reset to ${offset}")
     }
 }
