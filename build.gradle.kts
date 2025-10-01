7e8758b2e6ee76699bd951ab7feb45a940f03d3e
Sven Obser <dev@brudaswen.de>
2025-10-01 17:13:44 +0200

    [CI] Update publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,9 +33,6 @@ nexusPublishing {
     clientTimeout = 5.minutes.toJavaDuration()
 
     repositories {
-        sonatype {
-            nexusUrl = uri("https://ossrh-staging-api.central.sonatype.com/service/local/")
-            snapshotRepositoryUrl = uri("https://central.sonatype.com/repository/maven-snapshots/")
-        }
+        sonatype()
     }
 }
