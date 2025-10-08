47fa1bf3c81944cc4c099c0bec7fbe474a042d7c
Sven Obser <dev@brudaswen.de>
2025-10-08 11:22:40 +0200

    Add JVM extensions

--- library/src/jvmMain/kotlin/kotlinx/serialization/csv/CsvRecordWriter.jvm.kt
+++ library/src/jvmMain/kotlin/kotlinx/serialization/csv/CsvRecordWriter.jvm.kt
@@ -14,6 +14,21 @@ import java.io.OutputStream
 @ExperimentalSerializationApi
 public fun <T : Any> Csv.recordWriter(
     serializer: KSerializer<T>,
+    output: Appendable,
+): CsvRecordWriter<T> = recordWriter(
+    serializer = serializer,
+    sink = CsvSink(output),
+)
+
+/**
+ * Create [CsvRecordWriter] that allows writing CSV line-by-line.
+ *
+ * @param serializer The serializer used to serialize the given object.
+ * @param output The output where the CSV will be written.
+ */
+@ExperimentalSerializationApi
+public fun <T : Any> Csv.recordWriter(
+    serializer: KSerializer<T>,
     output: OutputStream,
 ): CsvRecordWriter<T> = recordWriter(
     serializer = serializer,
